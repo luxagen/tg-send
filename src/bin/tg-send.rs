4963160e5c3c070a11e27bc7aee871581b40db41
luxagen <hello@luxagen.com>
2023-07-08 19:05:54 +0100

    rough initial commit

---
+++ src/bin/tg-send.rs
@@ -0,0 +1,27 @@
+#![allow(unreachable_code)]
+
+use std::env;
+
+extern crate tokio;
+use futures::StreamExt;
+use telegram_bot::*;
+use std::io::Read;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+	let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+	let id = env::var("TELEGRAM_CHANNEL_ID").expect("TELEGRAM_CHANNEL_ID not set");
+
+	let api = Api::new(token);
+
+	let chat = ChannelId::new(id.parse::<i64>().unwrap());
+
+	let mut buf = String::new();
+	let s = std::io::BufReader::new(std::io::stdin()).read_to_string(&mut buf);
+	let text = std::borrow::Cow::Owned(buf);
+	api.spawn(chat.text(text));
+
+	std::thread::sleep(std::time::Duration::from_millis(2000));
+
+	Ok(())
+}
