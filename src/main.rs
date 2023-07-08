196fd0b61ca798dc1f4cce427e11fa5fe5d418d2
luxagen <hello@luxagen.com>
2023-07-08 20:50:53 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,23 @@ use tokio;
 use telegram_bot::*;
 use std::io::Read;
 
+fn slurp_stdin() -> String
+{
+	let mut buf = String::new();
+
+	std::io::BufReader::new(std::io::stdin()).read_to_string(&mut buf).unwrap();
+
+	return buf;
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
 	let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-	let id = env::var("TELEGRAM_CHANNEL_ID").expect("TELEGRAM_CHANNEL_ID not set");
+	let channel_id = env::var("TELEGRAM_CHANNEL_ID").expect("TELEGRAM_CHANNEL_ID not set").parse::<i64>().unwrap();
 
-	let api = Api::new(token);
-
-	let chat = ChannelId::new(id.parse::<i64>().unwrap());
-
-	let mut buf = String::new();
-	std::io::BufReader::new(std::io::stdin()).read_to_string(&mut buf).unwrap();
-	let text = std::borrow::Cow::Owned(buf);
-	let t = api.send(chat.text(text));
-
-	t.await.unwrap();
+	let chat = ChannelId::new(channel_id);
+	let text = std::borrow::Cow::Owned(slurp_stdin());
+	Api::new(token).send(chat.text(text)).await.unwrap();
 
 	Ok(())
 }
