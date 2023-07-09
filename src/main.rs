0c73e8a149b96f252707154ef6a389352b7fd395
luxagen <hello@luxagen.com>
2023-07-09 11:30:40 +0100

    Command-line support with proper colouring

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,104 @@ use tokio;
 use telegram_bot::*;
 use std::io::Read;
 
+#[derive(clap::Subcommand)]
+enum Commands
+{
+	SendText { chatid: i64 },
+}
+
+#[derive(clap::Parser)]
+#[clap(author,about,long_about=None)]
+#[command(arg_required_else_help = true)]
+struct Args
+{
+	#[arg(long,short,help="Commands will be performed via this Telegram bot token")]
+	token: Option<String>,
+
+	#[arg(long,short,help="Wait this number of milliseconds for a response from Telegram, otherwise wait indefinitely")]
+	wait: Option<u64>,
+
+	#[command(subcommand,help="Send a text message via stdin")]
+	command: Commands,
+}
+
+const EVK_TBT: &str = "TELEGRAM_BOT_TOKEN";
+
+impl Args
+{
+	fn parse() -> Self
+	{
+		let mut s = <Self as clap::Parser>::parse();
+
+		let mut errors = Vec::<String>::new();
+
+		use colored::*;
+
+		if s.token.is_none()
+		{
+			match env::var(EVK_TBT)
+			{
+				Err(_) => errors.push(format!("No bot token provided [{}] and {} is not set","-t".yellow(),EVK_TBT.green())),
+				Ok(t) => s.token = Some(t), // TODO move this code to main() ?
+			}
+		}
+
+		if !errors.is_empty()
+		{
+			for e in errors
+			{
+				eprintln!("{} {e}","error:".bright_red().bold());
+			}
+
+			std::process::exit(2);
+		}
+
+		s
+	}
+}
+
+lazy_static::lazy_static!
+{
+	static ref ARGS: Args = Args::parse();
+}
+
 fn slurp_stdin() -> String
 {
 	let mut buf = String::new();
 
-	std::io::BufReader::new(std::io::stdin()).read_to_string(&mut buf).unwrap();
-
-	return buf;
+	match std::io::BufReader::new(std::io::stdin()).read_to_string(&mut buf)
+	{
+		Err(_) => panic!(),
+		Ok(_) => buf,
+	}
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-	let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-	let channel_id = env::var("TELEGRAM_CHANNEL_ID").expect("TELEGRAM_CHANNEL_ID not set").parse::<i64>().unwrap();
+	let api = Api::new(ARGS.token.as_ref().unwrap());
 
-	let chat = ChannelId::new(channel_id);
-	let text = std::borrow::Cow::Owned(slurp_stdin());
-	Api::new(token).send(chat.text(text)).await.unwrap();
+	match ARGS.command
+	{
+		Commands::SendText { chatid } =>
+		{
+			let chat = ChannelId::new(chatid);
+			let text = std::borrow::Cow::Owned(slurp_stdin());
+
+			let wait = match ARGS.wait
+			{
+				None => core::time::Duration::from_secs(20),
+				Some(x) => core::time::Duration::from_millis(x),
+			};
+
+			let future = api.send_timeout(chat.text(text),wait);
+
+			match future.await
+			{
+				Err(e) => eprintln!("error: {e}"),
+				_ => std::process::exit(0),
+			}
+		},
+	}
 
 	Ok(())
 }
